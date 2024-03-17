5200c02892540f28bedf2c9751baa169b773e5ae
Johnny Steenbergen <jwberged@gmail.com>
2024-03-17 17:52:07 -0500

    chore: update doc.go to include note about issues with hot paths

--- doc.go
+++ doc.go
@@ -3,6 +3,10 @@
 // inspired by the [upspin project's error handling](https://github.com/upspin/upspin/blame/master/errors/errors.go)
 // and github.com/pkg/errors. It also adds a few lessons learned
 // from creating a module like this a number of times across numerous
-// projects.
+// projects. Worth noting here, this pkg has some limitations, like in
+// hot loops. In that case, you may want to use std lib errors or
+// similar for the hot loop, then return the result of those with
+// this module's error handling with a simple:
+// 	errors.Wrap(hotPathErr)
 
 package errors
